a3812f3ffd8d09a6d6e64f93fd4ddafffd56a941
mariadvera <maliave213@gmail.com>
2024-10-22 10:03:04 -0500

    feat : hicimos la prueba de la clase pong

--- pong.py
+++ pong.py
@@ -1,32 +1,41 @@
 import pygame
 
-pygame.init()
-pantalla = pygame.display.set_mode((800,600))
+class Pong:
+    
 
-salir =False
-cont =0
+    def __init__(self):
+        pygame.init()
+        self.pantalla = pygame.display.set_mode((800,600))
 
-while not salir:    #bucle principal (main loop) 
-    cont = cont +1
-    print(cont)
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-          # print('Se ha cerrado la ventana')
-           salir = True
-        
-   # print('se ha producido un evento del tipo:', evento)
+    def jugar(self):
+        salir =False
+        cont =0
 
-    # renderizar mis objetos
-    # 1. borrar la pantalla
-    pygame.draw.rect(pantalla, (0,0,0), ((0,0), (800,600)) )
+        while not salir:    #bucle principal (main loop) 
+            cont = cont +1
+          
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                # print('Se ha cerrado la ventana')
+                  salir = True
+                
+        # print('se ha producido un evento del tipo:', evento)
 
-    # 2. pintar los objetos en su nueva posición
+            # renderizar mis objetos
+            # 1. borrar la pantalla
+            pygame.draw.rect(self.pantalla, (0,0,0), ((0,0), (800,600)) )
 
-    rectangulo = pygame.Rect(50,50,300,150)
-    pygame.draw.rect(pantalla,(cont % 255, 68,158), rectangulo )
+            # 2. pintar los objetos en su nueva posición
 
-    # mostrar los cambios en la pantalla
-    pygame.display.flip()
+            rectangulo = pygame.Rect(50,50,300,150)
+            pygame.draw.rect(self.pantalla,(cont % 255, 68,158), rectangulo )
+
+            # mostrar los cambios en la pantalla
+            pygame.display.flip()
 
 
-pygame.quit()
+        pygame.quit()
+
+if __name__== '__main__':
+    juego =Pong()
+    juego.jugar()
